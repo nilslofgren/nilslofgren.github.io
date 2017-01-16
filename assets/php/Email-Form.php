f1baa7b37fccb00e773b0b106b68e619e55282af
Nils Lofgren <nlofgren@villanova.edu>
2017-01-16 12:17:25 -0500

    Update Email-Form.php

--- assets/php/Email-Form.php
+++ assets/php/Email-Form.php
@@ -28,4 +28,4 @@ $to = "nilslofgren@nilslofgren.me";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-header('Location: thank-you.html');
+header('Location: index-revised.html');

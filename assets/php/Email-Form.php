611ce3709504f1da00d5f63b08871d7667684ff2
Nils Lofgren <nlofgren@villanova.edu>
2017-01-16 12:10:52 -0500

    Update Email-Form.php

--- assets/php/Email-Form.php
+++ assets/php/Email-Form.php
@@ -1,91 +1,15 @@
 <?php
-if(isset($_POST[$_POST['email']])) {
+    $name = $_POST['first_name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['notes'];
 
-    $email_to = "nilslofgren@nilslofgren.me";
-    $email_subject = "Contact Request";
+    $email_from = 'yourname@yourwebsite.com';
+    $email_subject = "New Form submission";
+    $email_body = "You have received a new message from the user $name.\n".
+                        "Here is the message:\n $message".
 
-}
-
-
-if(isset($_POST[$_POST['email']])) {
-
-$email_to = "nilslofgren@nilslofgren.me";
-$email_subject = "Contact Request";
-
-}
-if(!isset($_POST['first_name'] ||
-    !isset($_POST['email'] ||
-    !isset($_POST['notes'])))) {
-        died('Error inputs not set');
-    }
-    $first_name = $_POST['first_name'];
-    $email = $_POST['email'];
-    $notes = $_POST['notes'];
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-if(!preg_match($email_exp,$email_from)) {
-
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-
-}
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-if(!preg_match($string_exp,$first_name)) {
-
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-
-}
-
-if(!preg_match($string_exp,$last_name)) {
-
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-
-}
-
-if(strlen($notes) < 2) {
-
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-
-}
-
-if(strlen($error_message) > 0) {
-
-    died($error_message);
-
-}
-function clean_string($string){
-    $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-    return str_replace($bad, "", $string);
-}
-
-
-$email_message .= "Name: ".clean_string($first_name)."\n";
-$email_message .= "Email Adress".clean_string($email_from)."\n";
-$email_message .= "Notes".clean_string($notes)."\n";
-
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n".
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);   
-
-function died($error) {
-
-    // your error code can go here
-
-    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-
-    echo "These errors appear below.<br /><br />";
-
-    echo $error."<br /><br />";
-
-    echo "Please go back and fix these errors.<br /><br />";
-
-    die();
-} 
-
+    $to = "nilslofgren@nilslofgren.me";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to, $email_subject, $email_body, $headers);
 ?>
-
-

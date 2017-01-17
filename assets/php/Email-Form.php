ade3c5bd21a591941e871365a144470478caa74b
Nils Lofgren <nlofgren@villanova.edu>
2017-01-16 22:45:46 -0500

    Update Email-Form.php

--- assets/php/Email-Form.php
+++ assets/php/Email-Form.php
@@ -1,31 +1,32 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	echo "error; you need to submit the form!";
-}
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+	if(!isset($_POST['submit']))
+	{
+		echo "error; you need to submit the form!";
+	}
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
 
-if(empty($name)||empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
+	if(empty($name)||empty($visitor_email)) 
+	{
+	    echo "Name and email are mandatory!";
+	    exit;
+	}
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+	if(IsInjected($visitor_email))
+	{
+	    echo "Bad email value!";
+	    exit;
+	}
 
-$email_from = 'nilslofgren@nilslofgren.me';
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
-    
-$to = "nilslofgren@nilslofgren.me";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header('Location: index-revised.html');
+	$email_from = 'nilslofgren@nilslofgren.me';
+	$email_subject = "New Form submission";
+	$email_body = "You have received a new message from the user $name.\n".
+	    "Here is the message:\n $message".
+
+	$to = "nilslofgren@nilslofgren.me";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
+	mail($to,$email_subject,$email_body,$headers);
+	header('Location: index-revised.html');
+?>
